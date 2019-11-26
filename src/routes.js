f7a3454ab9fb0e3233a8647665d5636da3f99221
Matheus Bonfiglioli <bonfiglioli.mat@gmail.com>
2019-11-26 14:43:28 -0300

    Initial structure finished

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import Home from './pages/Home';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    Home,
+  })
+);
